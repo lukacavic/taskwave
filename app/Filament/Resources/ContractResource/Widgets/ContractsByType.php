ddd8860009a1cbbba5aac322c6d4a6b4d1783af7
lukacavic <luka.cavic@gmail.com>
2024-10-10 09:22:46 +0200

    Fix contract type chart

--- app/Filament/Resources/ContractResource/Widgets/ContractsByType.php
+++ app/Filament/Resources/ContractResource/Widgets/ContractsByType.php
@@ -17,11 +17,11 @@ class ContractsByType extends ChartWidget
         return [
             'datasets' => [
                 [
-                    'label' => 'Blog posts created',
-                    'data' => [0, 10, 5, 2, 21, 32, 45, 74, 65, 45, 77, 89],
+                    'label' => 'Contracts by type',
+                    'data' => [10, 5, 2, 11],
                 ],
             ],
-            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            'labels' => ['Express Contracts', 'Adhesion Contracts', 'Implied Contracts', 'Contracts under seal'],
         ];
     }
 
